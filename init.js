b46875ca9e11507174aca7abf19367600326a544
LUIS D CAMACHO V <lui.7447@gmail.com>
2017-05-15 21:18:53 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,86 @@
+function init() {
+	root = {x:0,y:0,label:"world",data:0,leaves:46,height:4,children:[
+			{x:0,y:0,label:"america",data:0,leaves:13,height:2,children:[
+				{x:0,y:0,label:"canada",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"otawa",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"quebec",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"montreal",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"mexico",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"monterrey",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"guadalajara",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"puebla",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"brazil",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"rio de janeiro",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"sao paulo",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"recife",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"costa rica",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"san jose",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"heredia",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"alajuela",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"cartago",data:0,leaves:1,height:0,children:[]}
+				]}
+			]},
+			{x:0,y:0,label:"europe",data:0,leaves:9,height:2,children:[
+				{x:0,y:0,label:"portugal",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"lisboa",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"oporto",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"braga",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"spain",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"madrid",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"barcelona",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"valencia",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"germany",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"berlin",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"hamburgo",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"munich",data:0,leaves:1,height:0,children:[]}
+				]}
+			]},
+			{x:0,y:0,label:"africa",data:0,leaves:9,height:2,children:[
+				{x:0,y:0,label:"camerun",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"duala",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"sudafrica",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"ciudad del cabo",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"egipto",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"cairo",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"alexandria",data:0,leaves:1,height:0,children:[]}
+				]}
+			]},
+			{x:0,y:0,label:"asia",data:0,leaves:9,height:2,children:[
+				{x:0,y:0,label:"india",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"bombay",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"delhi",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"bangalore",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"rusia",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"moscu",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"san petersburgos",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"novosibirsk",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"china",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"shanghai",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"pekin",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"canton",data:0,leaves:1,height:0,children:[]}
+				]}
+			]},
+			{x:0,y:0,label:"oceania",data:0,leaves:5,height:1,children:[
+				{x:0,y:0,label:"australia",data:0,leaves:2,height:1,children:[
+					{x:0,y:0,label:"sydney",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"melbourne",data:0,leaves:1,height:0,children:[]}
+				]},
+				{x:0,y:0,label:"nueva zelanda",data:0,leaves:3,height:1,children:[
+					{x:0,y:0,label:"Isla del Norte",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"Isla del Sur",data:0,leaves:1,height:0,children:[]},
+					{x:0,y:0,label:"Islas Catham",data:0,leaves:1,height:0,children:[]}
+				]}
+			]}
+		]
+	}
+	return root;
+}
